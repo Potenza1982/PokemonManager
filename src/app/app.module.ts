c198cb3f2f6a7f32375c98fe4828ef25172c9151
Potenza1982 <mabarragancampos@gmail.com>
2024-02-02 15:27:11 +0100

    DWS_TEMA7_PRACTICA - ANGULAR FORMATEO EN PLANTILLAS - ARRAY - COLUMNAS - PESO Y ALTURA CON  PIPE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
-import { NgModule } from '@angular/core';
+// Importaciones desde Angular core
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+// Importaciones desde Angular platform-browser
 import { BrowserModule } from '@angular/platform-browser';
 
+// Importación de CommonModule desde Angular common
+import { CommonModule } from '@angular/common';
+
+// Importación del módulo de rutas AppRoutingModule
 import { AppRoutingModule } from './app-routing.module';
+
+// Importación de componentes
 import { AppComponent } from './app.component';
 import { GeneracionesComponent } from './componentes/generaciones/generaciones.component';
 import { PokemonsComponent } from './componentes/pokemons/pokemons.component';
 
+// Importación de datos de localización
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+registerLocaleData(es);
+
+// Decorador NgModule que define el módulo principal de la aplicación
 @NgModule({
-  declarations: [
-    AppComponent,
-    GeneracionesComponent,
-    PokemonsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  // Declaración de componentes que pertenecen al módulo
+  declarations: [AppComponent, GeneracionesComponent, PokemonsComponent],
+
+  // Importación de otros módulos que se necesitan en este módulo
+  imports: [BrowserModule, AppRoutingModule, CommonModule],
+
+  // Configuración de proveedores de servicios
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
+
+  // Componente principal que inicia la aplicación
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+// Clase del módulo principal de la aplicación
+export class AppModule {}
--- src/app/componentes/pokemons/pokemons.component.ts
+++ src/app/componentes/pokemons/pokemons.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+// Importación del decorador Component y OnInit desde Angular core
+import { Component, OnInit } from '@angular/core';
 
+// Decorador Component con metadatos que definen el componente
 @Component({
+  // Selector del componente, cómo será llamado en otros archivos HTML
   selector: 'app-pokemons',
-  templateUrl: './pokemons.component.html',
-  styleUrl: './pokemons.component.css'
-})
-export class PokemonsComponent {
 
+  // Ruta al archivo HTML que define la estructura del componente
+  templateUrl: './pokemons.component.html',
+
+  // Ruta al archivo de estilos del componente
+  styleUrls: ['./pokemons.component.css'],
+})
+// Clase del componente que implementa la interfaz OnInit
+export class PokemonsComponent implements OnInit {
+  // Array de Pokémon con los datos proporcionados
+  pokemons = [
+    {
+      id: 1,
+      nombre: 'Bulbasaur',
+      categoria: undefined, // Categoría no especificada inicialmente
+      tipo: 'Planta y Venenosa',
+      talla: 0.7, // Altura en metros
+      peso: 6.9, // Peso en kilogramos
+    },
+    {
+      id: 2,
+      nombre: 'Ivysaur',
+      categoria: undefined, // Categoría no especificada inicialmente
+      tipo: 'Planta y Venenosa',
+      talla: 1, // Altura en metros
+      peso: 13, // Peso en kilogramos
+    },
+    {
+      id: 3,
+      nombre: 'Venusaur',
+      categoria: undefined, // Categoría no especificada inicialmente
+      tipo: 'Planta y Venenosa',
+      talla: 2, // Altura en metros
+      peso: 100, // Peso en kilogramos
+    },
+    {
+      id: 4,
+      nombre: 'Charmander',
+      categoria: 'Lagarto', // Categoría especificada
+      tipo: 'Fuego',
+      talla: 0.6, // Altura en metros
+      peso: 8.5, // Peso en kilogramos
+    },
+    {
+      id: 5,
+      nombre: 'Charmaleon',
+      categoria: undefined, // Categoría no especificada inicialmente
+      tipo: 'Fuego',
+      talla: 1.1, // Altura en metros
+      peso: 19, // Peso en kilogramos
+    },
+    {
+      id: 6,
+      nombre: 'Charizard',
+      categoria: undefined, // Categoría no especificada inicialmente
+      tipo: 'Fuego y Vuelo',
+      talla: 1.7, // Altura en metros
+      peso: 90.5, // Peso en kilogramos
+    },
+  ];
+
+  // Constructor del componente
+  constructor() {}
+
+  // Método OnInit que se ejecuta cuando el componente se inicializa
+  ngOnInit() {}
 }
